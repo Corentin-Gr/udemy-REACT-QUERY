ae4512ac860ed444b05d678043e5e7c6d675a0ae
CoGregoire <corentin.gregoire@gmail.com>
2026-02-09 14:22:09 +0100

    Section 8

--- base-lazy-days/client/src/components/appointments/hooks/useCancelAppointment.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useCancelAppointment.ts
@@ -1,22 +1,33 @@
-import { Appointment } from "@shared/types";
+import { Appointment } from '@shared/types';
 
-import { axiosInstance } from "@/axiosInstance";
-import { useCustomToast } from "@/components/app/hooks/useCustomToast";
-import { queryKeys } from "@/react-query/constants";
+import { axiosInstance } from '@/axiosInstance';
+import { useCustomToast } from '@/components/app/hooks/useCustomToast';
+import { queryKeys } from '@/react-query/constants';
+import { useMutation } from '@tanstack/react-query';
+import { queryClient } from '@/react-query/queryClient';
 
-// for when server call is needed
-// async function removeAppointmentUser(appointment: Appointment): Promise<void> {
-//   const patchData = [{ op: 'remove', path: '/userId' }];
-//   await axiosInstance.patch(`/appointment/${appointment.id}`, {
-//     data: patchData,
-//   });
-// }
+async function removeAppointmentUser(appointment: Appointment): Promise<void> {
+    const patchData = [{ op: 'remove', path: '/userId' }];
+    await axiosInstance.patch(`/appointment/${appointment.id}`, {
+        data: patchData,
+    });
+}
 
 export function useCancelAppointment() {
-  const toast = useCustomToast();
+    const toast = useCustomToast();
 
-  // TODO: replace with mutate function
-  return (appointment: Appointment) => {
-    // nothing to see here
-  };
+    const { mutate } = useMutation({
+        mutationFn: removeAppointmentUser,
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: [queryKeys.appointments],
+            });
+            toast({
+                title: 'You have cancelled an appointment',
+                status: 'success',
+            });
+        },
+    });
+
+    return mutate
 }
